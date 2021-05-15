a940dad5ecb3bdd4d724864bbfc179451ae2e8b9
mjls130598 <mjls130598@gmail.com>
2021-05-15 11:06:01 +0200

    Almacena datos.csv en BBDD

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM python:3.7-alpine
 ENV PYTHONUNBUFFERED=1
 WORKDIR /code
 COPY . /code/
+RUN apk add --no-cache --update gcc g++\
+    gfortran musl-dev
 RUN pip install -r requirements.txt
--- enviarBBDD.py
+++ enviarBBDD.py
@@ -1,19 +1,17 @@
-from mongoengine import connect, EmbeddedDocument	
+from mongoengine import connect, Document	
 from mongoengine.fields import FloatField, DateTimeField
 from datetime import datetime
-import panda as pd
+import pandas as pd
 
 connect('practica2', host='mongo')
 
-class Datos(EmbeddedDocument):
-    fecha = DateTimeField(default=datetime.datetime.now)
+class Datos(Document):
+    fecha = DateTimeField(default=datetime.now())
     temperatura = FloatField(default=0.0)
     humedad = FloatField(default=0.0)
 
 datos = pd.read_csv("./datos.csv")
 
 for index, row in datos.iterrows():
-    Datos(fecha=row["datatime"], temperatura=row["temperature"], humedad=row["humidity"]).save()
-
-for dato in Datos.objects.all():
-    print(dato.fecha)
+    datos = Datos(fecha=row["datetime"], temperatura=row["temperature"], humedad=row["humidity"])
+    datos.save()
